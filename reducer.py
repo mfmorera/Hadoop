ee41c85ce07c40d7ea8ef06f1542a6b5857d95bd
mfmorera <moreramoralesm@gmail.com>
2025-04-02 17:47:56 -0500

    Add files via upload

---
+++ reducer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+from collections import defaultdict
+
+word_count = defaultdict(int)
+
+for line in sys.stdin:
+    word, count = line.strip().split("\t")
+    word_count[word] += int(count)
+
+for word, count in word_count.items():
+    print(f"{word}\t{count}")
+
